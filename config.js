b6e5f198732d90657a8558209ef5425773464347
Pedro Douglas Alves Braga <pedro.douglas@academico.ufpb.br>
2026-09-20 15:07:22 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const CONFIG = {
   SUPABASE_URL: "https://tixlqodhusvhfhcnwyvp.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_bVSwEBZotI6KNFaGufpKuA_S2ZuNFRk",
-  CONSENT_VERSION: "TCLE-v2.0-2026-09-20",
+  CONSENT_VERSION: "TCLE-2026-09-20",
   CONSENT_URL: "./tcle.html",
-  COLLECTION_ENABLED: false,
+  COLLECTION_ENABLED: true,
   REQUIRED_RATERS: 6,
   I_CVI_THRESHOLD: 0.78,
   S_CVI_THRESHOLD: 0.90,
